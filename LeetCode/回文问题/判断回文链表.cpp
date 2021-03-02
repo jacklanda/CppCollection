b27da2cfa5574c83828c6a7d4fe4d60d82ffc12c
jacklanda <yonyonlau@gmail.com>
2021-03-02 13:24:20 +0800

    update 判断回文链表

--- LeetCode/回文问题/判断回文链表.cpp
+++ LeetCode/回文问题/判断回文链表.cpp
@@ -27,3 +27,48 @@ public:
         return res;
     }
 };
+
+/* 法二：优化空间复杂度法（利用回文链表的对称性+反转链表）*/
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        ListNode* fast = head;
+        ListNode* slow = head;
+        while (fast != nullptr && fast->next != nullptr) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        if (fast != nullptr) slow = slow->next;
+
+        ListNode* left = head;
+        ListNode* right = reverse(slow);
+        while (right) {
+            if (left->val != right->val) return false;
+            left = left->next;
+            right = right->next;
+        }
+        return true;
+    }
+
+    ListNode* reverse(ListNode* head) {
+        ListNode* pre = nullptr;
+        ListNode* cur = head;
+        while (cur) {
+            ListNode* tmp = cur->next;
+            cur->next = pre;
+            pre = cur;
+            cur = tmp;
+        }
+        return pre;
+    }
+};
