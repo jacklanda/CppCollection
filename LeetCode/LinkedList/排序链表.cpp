6fee415a47277f8a8b2ead7be031c07b550eee64
jacklanda <yonyonlau@gmail.com>
2021-03-26 09:24:49 +0800

    update 排序链表

--- LeetCode/LinkedList/排序链表.cpp
+++ LeetCode/LinkedList/排序链表.cpp
@@ -9,7 +9,8 @@
  * };
  */
 
-/* 两次遍历，
+/* 解法一：
+ * 两次遍历，
  * 使用STL内置的基于
  * 堆排序的优先级队列 */
 class Solution {
@@ -30,3 +31,36 @@ public:
         return head;
     }
 };
+
+/* 解法二：
+ * 归并排序（递归式->自顶向下）
+ * 时空复杂度：O(nlogn), O(logn)
+ * */
+class Solution {
+public:
+    ListNode* sortList(ListNode* head) {
+        if (head == nullptr || head->next == nullptr) return head;
+        ListNode *slow = head, *fast = head, *pre = head;
+        while (fast && fast->next) {
+            //记录slow的前驱，针对链表有偶数个结点的情况
+            pre = slow;
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        pre->next = nullptr;
+        //明确递归函数的返回结果是什么，sortList的返回结果就是已经排序好的链表的头节点；而merge的返回结果就是两个链表合并好之后的链表的头节点
+        return merge(sortList(head), sortList(slow));
+    }
+    ListNode* merge(ListNode* l1, ListNode* l2) {
+        if (l1 == nullptr) return l2;
+        if (l2 == nullptr) return l1;
+        if (l1->val <= l2->val) {
+            l1->next = merge(l1->next, l2);
+            return l1;
+        } else {
+            l2->next = merge(l1, l2->next);
+            return l2;
+        }
+    }
+};
+
