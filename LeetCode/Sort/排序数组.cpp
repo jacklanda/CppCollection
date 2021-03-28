5d3619bbc82048f5015a4e8312b31575eb9d5dce
jacklanda <yonyonlau@gmail.com>
2021-03-28 12:24:45 +0800

    add 排序数组

---
+++ LeetCode/Sort/排序数组.cpp
@@ -0,0 +1,25 @@
+/* 未经优化过的快速排序 */
+class Solution {
+public:
+    vector<int> sortArray(vector<int>& nums) {
+        quickSort(nums, 0, nums.size() - 1);
+        return nums;
+    }
+    void quickSort(vector<int>& nums, int low, int high) {
+        if (low >= high) return;
+        int mid = partition(nums, low, high);
+        quickSort(nums, low, mid - 1);
+        quickSort(nums, mid + 1, high);
+    }
+    int partition(vector<int>& nums, int low, int high) {
+        int pivot = nums[low];
+        while (low < high) {
+            while (low < high && pivot <= nums[high]) high--;
+            nums[low] = nums[high];
+            while (low < high && pivot >= nums[low]) low++;
+            nums[high] = nums[low];
+        }
+        nums[low] = pivot;
+        return low;
+    }
+};
