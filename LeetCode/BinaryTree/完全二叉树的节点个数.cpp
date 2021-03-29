bf35f35e5d0d850b80d71702c3866511f9ddc687
jacklanda <yonyonlau@gmail.com>
2021-03-29 21:45:50 +0800

    add 完全二叉树的节点个数

---
+++ LeetCode/BinaryTree/完全二叉树的节点个数.cpp
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int countNodes(TreeNode* root) {
+        if (!root) return 0;
+
+        int left_num = countNodes(root->left);
+        int right_num = countNodes(root->right);
+
+        return left_num + right_num + 1;
+    }
+};
