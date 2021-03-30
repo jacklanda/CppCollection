802c6f420243a7d21f3484cb217d033d58cb401d
jacklanda <yonyonlau@gmail.com>
2021-03-30 15:50:28 +0800

    add 三数之和

---
+++ LeetCode/双指针/三数之和.cpp
@@ -0,0 +1,41 @@
+/* 双指针法是该问题的最优解法
+ * 时间复杂度：O(n^2) */
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> result;
+        sort(nums.begin(), nums.end());
+        // 找出a + b + c = 0：a = nums[i], b = nums[left], c = nums[right]
+        for (int i = 0; i < nums.size(); i++) {
+            // 排序之后如果第一个元素已经大于零，那么无论如何组合都不可能凑成三元组，直接返回结果就可以了
+            if (nums[0] > 0) {
+                return result;
+            }
+            // 正确去重方法
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            int left = i + 1;
+            int right = nums.size() - 1;
+            while (right > left) {
+                if (nums[i] + nums[left] + nums[right] > 0) {
+                    right--;
+                } else if (nums[i] + nums[left] + nums[right] < 0) {
+                    left++;
+                } else {
+                    result.push_back(
+                        vector<int>{nums[i], nums[left], nums[right]});
+                    // 去重逻辑应该放在找到一个三元组之后
+                    while (right > left && nums[right] == nums[right - 1])
+                        right--;
+                    while (right > left && nums[left] == nums[left + 1]) left++;
+                    // 找到答案时，双指针同时收缩
+                    right--;
+                    left++;
+                }
+            }
+        }
+        return result;
+    }
+};
+
