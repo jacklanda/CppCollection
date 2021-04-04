474a462604c03985e03e17e44e653dcec088b0c3
jacklanda <yonyonlau@gmail.com>
2021-04-04 23:58:31 +0800

    update 从尾到头打印链表

--- LeetCode/LinkedList/从尾到头打印链表.cpp
+++ LeetCode/LinkedList/从尾到头打印链表.cpp
@@ -7,6 +7,33 @@
  * };
  */
 
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+
+/* 通过栈容器的压/弹栈实现 */
+class Solution {
+public:
+    vector<int> reversePrint(ListNode* head) {
+        stack<int> stk;
+        vector<int> vec;
+        while (head) {
+            stk.push(head->val);
+            head = head->next;
+        }
+        while (!stk.empty()) {
+            vec.push_back(stk.top());
+            stk.pop();
+        }
+        return vec;
+    }
+};
+
 /* 以函数栈帧来模拟压栈 */
 class Solution {
 public:
@@ -22,3 +49,4 @@ public:
         return;
     }
 };
+
