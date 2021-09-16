567d1d57dc31fede3d0517b2ed8f36af32884563
jacklanda <yonyonlau@gmail.com>
2021-09-16 15:26:40 +0800

    update 合并两个有序链表

--- LeetCode/LinkedList/合并两个有序链表.cpp
+++ LeetCode/LinkedList/合并两个有序链表.cpp
@@ -8,6 +8,10 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
+/* 递归法: */
+/* 时间复杂度: O(n) */
+/* 空间复杂度: O(n); */
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
@@ -19,5 +23,37 @@ public:
         }
         l2->next = mergeTwoLists(l1, l2->next);
         return l2;
+    }
+};
+
+/* 迭代法 */
+/* 时间复杂度: O(n), n为链表长度 */
+/* 空间复杂度: O(1) */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        ListNode* dmy = new ListNode(0);  // 定义一个虚拟头结点
+        ListNode* cur = dmy;
+
+        if (!l1)
+            return l2;
+        else if (!l2)
+            return l1;
+        else if (!l1 && !l2)
+            return nullptr;
+
+        while (l1 && l2) {
+            if (l1->val <= l2->val) {
+                cur->next = l1;
+                l1 = l1->next;
+            } else if (l2->val < l1->val) {
+                cur->next = l2;
+                l2 = l2->next;
+            }
+            cur = cur->next;
+        }
+        cur->next = l1 ? l1 : l2;
+
+        return dmy->next;
     }
 };
