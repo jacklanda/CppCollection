73f39d280e815f09ecef4f33d4aa23a765877135
jacklanda <yonyonlau@gmail.com>
2021-09-17 17:41:02 +0800

    update 数组中出现次数超过一半的数字

--- LeetCode/Array/数组中出现次数超过一半的数字.cpp
+++ LeetCode/Array/数组中出现次数超过一半的数字.cpp
@@ -1,4 +1,17 @@
-/* 时间复杂度: O(nlogn+n) */
+/* 数组排序法 */
+/* 时间复杂度: O(nlogn) */
+/* 空间复杂度: O(1) */
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        std::sort(nums.begin(), nums.end());
+        int pos = nums.size() >> 1;
+        return nums[pos];
+    }
+};
+
+/* 哈希表统计法 */
+/* 时间复杂度: O(nlogn) */
 /* 空间复杂度: O(n) */
 class Solution {
 public:
@@ -10,13 +23,24 @@ public:
             else
                 m[n] = 1;
         }
-        std::vector<std::pair<int, int>> vec(m.begin(), m.end());
-        sort(vec.begin(), vec.end(), [](auto x, auto y) {
-            if (x.second == y.second)
-                return x.first < y.first;
-            else
-                return x.second > y.second;
-        });
-        return (*vec.begin()).first;
+        auto max_pair = std::max_element(
+            m.begin(), m.end(),
+            [](auto p1, auto p2) { return p1.second < p2.second; });
+        return (*max_pair).first;
+    }
+};
+
+/* 摩尔投票法 */
+/* 时间复杂度: O(n) */
+/* 空间复杂度: O(1) */
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int x, votes = 0;
+        for (int n : nums) {
+            if (votes == 0) x = n;
+            votes += n == x ? 1 : -1;
+        }
+        return x;
     }
 };
