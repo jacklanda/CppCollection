33df1e92277303b1a43a2b282f7c28894e02cdd1
jacklanda <yonyonlau@gmail.com>
2021-09-24 15:56:01 +0800

    update 最大自序和

--- LeetCode/DP/最大子序和.cpp
+++ LeetCode/DP/最大子序和.cpp
@@ -12,3 +12,19 @@ public:
         return max_sum;
     }
 };
+
+/* 贪心算法 */
+/* 时间复杂度: O(n) */
+/* 空间复杂度: O(1) */
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int sum = 0, max_sum = INT_MIN;
+        for (auto&& n : nums) {
+            sum += n;
+            max_sum = std::max(max_sum, sum);
+            if (sum < 0) sum = 0;
+        }
+        return max_sum;
+    }
+};
