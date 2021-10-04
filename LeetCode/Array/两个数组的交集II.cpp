a74d367828e62f546da2c0a20e63d0c175148d39
jacklanda <yonyonlau@gmail.com>
2021-10-04 17:30:33 +0800

    update 两个数组的交集II

--- LeetCode/Array/两个数组的交集II.cpp
+++ LeetCode/Array/两个数组的交集II.cpp
@@ -18,3 +18,25 @@ public:
         return vec;
     }
 };
+
+/* 以空间换时间
+ * 使用一个辅助的哈希重复集合
+ **/
+/* 时间复杂度: O(n) */
+/* 空间复杂度: O(n) */
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        std::unordered_multiset<int> s(nums1.begin(), nums1.end());
+        std::vector<int> vec;
+        vec.reserve(std::max(nums1.size(), nums2.size()));
+        for (auto& n : nums2) {
+            auto it = s.find(n);
+            if (it != s.end() && *it != INT_MAX) {
+                vec.emplace_back(n);
+                s.erase(it);
+            }
+        }
+        return vec;
+    }
+};
