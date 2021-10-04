0b48d661603ed0928447bab3c58256f06bb28bea
jacklanda <yonyonlau@gmail.com>
2021-10-04 17:45:20 +0800

    update 两个数组的交集II

--- LeetCode/Array/两个数组的交集II.cpp
+++ LeetCode/Array/两个数组的交集II.cpp
@@ -1,6 +1,7 @@
 /* 暴力解法 */
+/* m, n 分别是数组num1和num2的长度 */
 /* 时间复杂度: O(n^2) */
-/* 空间复杂度: O(n) */
+/* 空间复杂度: O(min(m,n)) */
 class Solution {
 public:
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
@@ -22,8 +23,8 @@ public:
 /* 以空间换时间
  * 使用一个辅助的哈希重复集合
  **/
-/* 时间复杂度: O(n) */
-/* 空间复杂度: O(n) */
+/* 时间复杂度: O(m+n) */
+/* 空间复杂度: O(m+n) */
 class Solution {
 public:
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
@@ -35,6 +36,30 @@ public:
             if (it != s.end() && *it != INT_MAX) {
                 vec.emplace_back(n);
                 s.erase(it);
+            }
+        }
+        return vec;
+    }
+};
+
+/* 排序+双指针 */
+/* 时间复杂度: O(mlogm + nlogn) */
+/* 空间复杂度: O(min(m,n)) */
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        std::sort(nums1.begin(), nums1.end());
+        std::sort(nums2.begin(), nums2.end());
+        std::vector<int> vec;
+        vec.reserve(std::max(nums1.size(), nums2.size()));
+        for (int i = 0, j = 0; i < nums1.size() && j < nums2.size();) {
+            if (nums1[i] == nums2[j]) {
+                vec.emplace_back(nums1[i]);
+                ++i, ++j;
+            } else if (nums1[i] < nums2[j]) {
+                ++i;
+            } else if (nums1[i] > nums2[j]) {
+                ++j;
             }
         }
         return vec;
