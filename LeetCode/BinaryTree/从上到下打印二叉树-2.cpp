ec24afea0554a8414c32474a4e8e31b91d1d550c
jacklanda <yonyonlau@gmail.com>
2021-10-23 15:24:44 +0800

    update 从上到下打印二叉树-2

--- LeetCode/BinaryTree/从上到下打印二叉树-2.cpp
+++ LeetCode/BinaryTree/从上到下打印二叉树-2.cpp
@@ -7,18 +7,23 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+
+/* 时间复杂度: O(n) */
+/* 空间复杂度: O(n) */
 class Solution {
 public:
     vector<vector<int>> levelOrder(TreeNode* root) {
-        vector<vector<int>> vec;
-        dfs(root, vec, 0);
-        return vec;
+        std::vector<std::vector<int>> res;
+        dfs(root, res, 0);
+        return res;
     }
-    void dfs(TreeNode* root, vector<vector<int>>& vec, int level) {
-        if(root == NULL) return ;
-        if(vec.size() <= level) vec.resize(level+1);    //level表示遍历到第几层
-        vec[level].push_back(root->val);
-        dfs(root->left, vec, level+1);
-        dfs(root->right, vec, level+1);
+    void dfs(TreeNode* root, std::vector<std::vector<int>>& vec, int level) {
+        if (root == nullptr) return;
+        if (vec.size() <= level)
+            vec.resize(level + 1);  // level 表示遍历到第几层
+        vec[level].emplace_back(root->val);
+        dfs(root->left, vec, level + 1);
+        dfs(root->right, vec, level + 1);
     }
 };
+
