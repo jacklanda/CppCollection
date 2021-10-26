7bbafd1431b7aabdf678f44d01c456ec340c6ed3
jacklanda <yonyonlau@gmail.com>
2021-10-26 12:55:57 +0800

    update 排序数组

--- LeetCode/Array/排序数组.cpp
+++ LeetCode/Array/排序数组.cpp
@@ -1,5 +1,9 @@
 /* 基于快速排序实现的数组排序：
-** 时间复杂度：O(nlogn) */
+ * 时间复杂度: O(nlogn)
+ * 空间复杂度: O(logn) => 递归调用的函数栈深度
+ * NOTE: 如果为全部有序数组
+ * 则时间复杂度将退化为 O(n^2) => 输入为大数组时容易超时!
+ * 因此我们需要随机选取枢轴 */
 class Solution {
 public:
     vector<int> sortArray(vector<int>& nums) {
@@ -13,6 +17,8 @@ public:
         quickSort(nums, mid + 1, high);
     }
     int partition(vector<int>& nums, int low, int high) {
+        int random_index = low + rand() % (high - low + 1);  // 随机选取枢轴，避免输入为有序数组时造成的快排退化
+        swap(nums[low], nums[random_index]);
         int pivot = nums[low];
         while (low < high) {
             while (low < high && pivot <= nums[high]) high--;
