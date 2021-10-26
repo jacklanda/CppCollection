b6a3bfcb35aa3f514ad0f24abd1d25a6aee84217
jacklanda <yonyonlau@gmail.com>
2021-10-26 14:00:10 +0800

    update 二叉树的中序遍历

--- LeetCode/BinaryTree/二叉树的中序遍历.cpp
+++ LeetCode/BinaryTree/二叉树的中序遍历.cpp
@@ -11,22 +11,46 @@
  * };
  */
 
-/* 简单地使用二叉树的
- * 递归框架思维即可解决问题
+/* 递归中序遍历二叉树
+ * 时间复杂度: O(n)
+ * 空间复杂度: O(n)
  * */
 class Solution {
-private:
-    vector<int> vec;
-
 public:
     vector<int> inorderTraversal(TreeNode* root) {
-        assist_func(root);
-        return vec;
+        traverse(root);
+        return res;
     }
-    void assist_func(TreeNode* root) {
-        if (!root) return;
-        assist_func(root->left);
-        vec.push_back(root->val);
-        assist_func(root->right);
+    void traverse(TreeNode* root) {
+        if (root == nullptr) return;
+        traverse(root->left);
+        res.emplace_back(root->val);
+        traverse(root->right);
+    }
+
+private:
+    vector<int> res;
+};
+
+/* 迭代中序遍历二叉树(用stack模拟栈帧) */
+/* 时间复杂度: O(n) */
+/* 空间复杂度: O(n) */
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> res;
+        stack<TreeNode*> stk;
+        TreeNode* cur = root;
+        while (cur != nullptr || !stk.empty()) {
+            while (cur != nullptr) {
+                stk.emplace(cur);
+                cur = cur->left;
+            }
+            cur = stk.top();
+            res.emplace_back(cur->val);
+            stk.pop();
+            cur = cur->right;
+        }
+        return res;
     }
 };
