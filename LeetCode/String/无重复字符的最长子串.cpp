4db4991d89f02aa5796795a19e1035d59c9bdbfe
jacklanda <yonyonlau@gmail.com>
2021-10-27 19:08:56 +0800

    update 无重复字符的最长子串

--- LeetCode/String/无重复字符的最长子串.cpp
+++ LeetCode/String/无重复字符的最长子串.cpp
@@ -1,15 +1,16 @@
 /* 拿数组当哈希表用
  * 然后进行滑动窗口 */
+/* 时间复杂度: O(n)  */
+/* 空间复杂度: O(k) => 只需128个int的连续空间(对应128个ascii字符) */
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        vector<int> hmap(128, 0);
-        int max_size = 0;
-        int left = 0;
-        for (int i = 0; i < s.size(); i++) {
-            left = max(left, hmap[s[i]]);
+        int hmap[128] = {0};
+        int left = 0, max_size = 0;
+        for (int i = 0; i < s.size(); ++i) {
+            left = std::max(left, hmap[s[i]]);
             hmap[s[i]] = i + 1;
-            max_size = max(max_size, i - left + 1);
+            max_size = std::max(max_size, i - left + 1);
         }
         return max_size;
     }
@@ -18,6 +19,8 @@ public:
 /* 滑动窗口
  * 利用哈希集合
  * 进行一次遍历 */
+/* 时间复杂度: O(n) */
+/* 空间复杂度: O(k) => 解释同上 */
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
