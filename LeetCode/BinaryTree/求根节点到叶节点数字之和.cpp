4277a3c7ef793e26986a9f48609d418d92b24299
jacklanda <yonyonlau@gmail.com>
2021-10-27 20:04:49 +0800

    add 求根节点到叶节点数字之和

---
+++ LeetCode/BinaryTree/求根节点到叶节点数字之和.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+/* DFS */
+/* 时间复杂度: O(n) */
+/* 空间复杂度: O(n) */
+class Solution {
+public:
+    int sumNumbers(TreeNode* root) {
+        if (root == nullptr) return 0;
+        dfs(root, 0);
+        return res;
+    }
+    void dfs(TreeNode* root, int num) {
+        num = num * 10 + root->val;
+        if (!root->left && !root->right) res += num;
+        if (root->left) dfs(root->left, num);
+        if (root->right) dfs(root->right, num);
+    }
+
+private:
+    int res;
+};
