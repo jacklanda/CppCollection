2912f6723a02da6ea87662ec2769f7aca60387e5
jacklanda <yonyonlau@gmail.com>
2021-10-29 14:14:26 +0800

    update 最小的k个数

--- LeetCode/Array/最小的k个数.cpp
+++ LeetCode/Array/最小的k个数.cpp
@@ -1,3 +1,14 @@
+/* 调用sort库函数 */
+/* 时间复杂度: O(nlogn) */
+/* 空间复杂度: O(logn) */
+class Solution {
+public:
+    vector<int> getLeastNumbers(vector<int>& arr, int k) {
+        std::sort(arr.begin(), arr.end());
+        return vector<int>(arr.begin(), arr.begin() + k);
+    }
+};
+
 /* 利用小根堆（优先级队列），依次弹出的k个数即为最小的k个数 */
 /* 时间复杂度:
  * O(n)+k*O(logn)，通过数组一次性构造std::priority_queue为O(n)，单次插入/删除操作为O(logn)
@@ -7,13 +18,42 @@
 class Solution {
 public:
     vector<int> getLeastNumbers(vector<int>& arr, int k) {
-        std::priority_queue<int, vector<int>, greater<int>> pq(arr.begin(),
-                                                               arr.end());
+        std::priority_queue<int, vector<int>, greater<int>> pq(arr.begin(), arr.end());
         std::vector<int> vec;
         while (k--) {
             vec.emplace_back(pq.top());
             pq.pop();
         }
         return vec;
+    }
+};
+
+/* 手动实现快排 */
+/* 时间复杂度: O(nlogn) */
+/* 空间复杂度: O(logn) */
+class Solution {
+public:
+    vector<int> getLeastNumbers(vector<int>& arr, int k) {
+        quickSort(arr, 0, arr.size() - 1);
+        return vector<int>(arr.begin(), arr.begin() + k);
+    }
+    void quickSort(vector<int>& arr, int low, int high) {
+        if (high <= low) return;
+        int midd = partition(arr, low, high);
+        quickSort(arr, low, midd - 1);
+        quickSort(arr, midd + 1, high);
+    }
+    int partition(vector<int>& arr, int low, int high) {
+        int random_idx = low + rand() % (high - low + 1);
+        std::swap(arr[low], arr[random_idx]);
+        int pivot = arr[low];
+        while (low < high) {
+            while (low < high && arr[high] >= pivot) --high;
+            arr[low] = arr[high];
+            while (low < high && arr[low] <= pivot) ++low;
+            arr[high] = arr[low];
+        }
+        std::swap(pivot, arr[low]);
+        return low;
     }
 };
--- LeetCode/BinaryTree/二叉树的最大深度.cpp
+++ LeetCode/BinaryTree/二叉树的最大深度.cpp
@@ -11,6 +11,7 @@
  * };
  */
 
+/* DFS */
 /* 时间复杂度：O(n)
  * 空间复杂度：O(n) */
 class Solution {
@@ -26,5 +27,31 @@ public:
         left_depth = recursiveTraverse(root->left, depth) + 1;
         right_depth = recursiveTraverse(root->right, depth) + 1;
         return max(left_depth, right_depth);
+    }
+};
+
+/* BFS */
+/* 时间复杂度: O(n) */
+/* 空间复杂度: O(n) */
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if (!root) return 0;
+        queue<TreeNode*> q;
+        vector<vector<int>> res;
+        q.emplace(root);
+        while (!q.empty()) {
+            int size = q.size();
+            vector<int> vec;
+            for (int i = 0; i < size; ++i) {
+                TreeNode* node = q.front();
+                q.pop();
+                vec.emplace_back(node->val);
+                if (node->left) q.emplace(node->left);
+                if (node->right) q.emplace(node->right);
+            }
+            res.emplace_back(vec);
+        }
+        return res.size();
     }
 };
