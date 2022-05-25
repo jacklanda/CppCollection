673859495cb03a1cd534d1f209b378b752f33228
jacklanda <yonyonlau@gmail.com>
2022-05-25 15:19:52 +0800

    update 448-FindAllNumbersDisappearedInAnArray

--- LeetCode/Array/448-FindAllNumbersDisappearedInAnArray.cpp
+++ LeetCode/Array/448-FindAllNumbersDisappearedInAnArray.cpp
@@ -5,6 +5,24 @@ class Solution {
 public:
     vector<int> findDisappearedNumbers(vector<int>& nums) {
         vector<int> res;
+        for (const int& num : nums) {
+            int idx = std::abs(num) - 1;
+            if (nums[idx] > 0) nums[idx] = -nums[idx];
+        }
+        for (int i = 0; i < nums.size(); ++i) {
+            if (nums[i] > 0) res.emplace_back(i + 1);
+        }
+        return res;
+    }
+};
+
+/* time complexity: O(n) */
+/* space complexity: O(n) */
+
+class Solution {
+public:
+    vector<int> findDisappearedNumbers(vector<int>& nums) {
+        vector<int> res;
         vector<int> buckets(nums.size() + 1, 0);
         for (const int& num : nums) ++buckets[num];
         for (int i = 1; i < buckets.size(); ++i)
