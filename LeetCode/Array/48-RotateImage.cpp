3d7e0bb2d30392044d274883faf53c99fe4daed0
jacklanda <yonyonlau@gmail.com>
2022-05-25 16:29:49 +0800

    update 48-RotateImage.cpp

--- LeetCode/Array/48-RotateImage.cpp
+++ LeetCode/Array/48-RotateImage.cpp
@@ -1,11 +1,11 @@
-/* 暴力法：使用一个辅助矩阵 */
-/* 时间复杂度: O(n^2) */
-/* 空间复杂度: O(n^2) */
+/* bad approach: use a assistant matrix */
+/* time complexity: O(n^2) */
+/* space complexity: O(n^2) */
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
         std::vector<vector<int>> assist_matrix = matrix;
-        for (int i = matrix.size() - 1, k = 0; i >= 0; --i, ++k) {  // i -> 行
+        for (int i = matrix.size() - 1, k = 0; i >= 0; --i, ++k) {  // i -> row
             for (int j = 0; j < matrix[0].size(); ++j) {
                 assist_matrix[j][k] = matrix[i][j];
             }
@@ -14,20 +14,21 @@ public:
     }
 };
 
-/* 两次翻转代替旋转 */
-/* 时间复杂度: O(n^2) */
-/* 空间复杂度: O(1) => 原地翻转 */
+/* two times flipping equal to rotation */
+/* time complexity: O(n^2) */
+/* space complexity: O(1) => rotate in-place */
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
         int row = matrix.size(), col = matrix[0].size();
-        for (int i = 0; i < (row >> 1); ++i) {  // 先根据水平轴线翻转
+        // flip on the horizontal axix
+        for (int i = 0; i < (row >> 1); ++i) {
             for (int j = 0; j < col; ++j) {
                 std::swap(matrix[i][j], matrix[row - i - 1][j]);
-                ;
             }
         }
-        for (int i = 0; i < row; ++i) {  // 再根据主对角线翻转
+        // flip on the main diagonal
+        for (int i = 0; i < row; ++i) {
             for (int j = 0; j < i; ++j) {
                 swap(matrix[i][j], matrix[j][i]);
             }
