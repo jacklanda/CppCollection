f0131dcae714230b6c223dd09c4f3a8fc99c2619
jacklanda <yonyonlau@gmail.com>
2022-06-15 09:17:50 +0800

    update 15-3Sum

--- LeetCode/Array/15-3Sum.cpp
+++ LeetCode/Array/15-3Sum.cpp
@@ -9,13 +9,13 @@ public:
         std::sort(nums.begin(), nums.end());
         for (int i = 0; i < nums.size(); ++i) {
             int j = i + 1, k = nums.size() - 1;
-            if (i >= 1 && nums[i] == nums[i - 1]) continue;
+            if (i >= 1 && nums[i] == nums[i - 1]) continue;  // dedup i
             while (j < k) {
-                if (j > i + 1 && nums[j] == nums[j - 1]) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) {  // dedup j
                     ++j;
                     continue;
                 }
-                if (k < nums.size() - 1 && nums[k] == nums[k + 1]) {
+                if (k < nums.size() - 1 && nums[k] == nums[k + 1]) {  // dedup k
                     --k;
                     continue;
                 }
