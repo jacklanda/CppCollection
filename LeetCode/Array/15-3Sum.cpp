0dce49bce88e61916542ab4a527e5cc4b594c6bf
jacklanda <yonyonlau@gmail.com>
2022-06-15 08:53:47 +0800

    add 15-3Sum

---
+++ LeetCode/Array/15-3Sum.cpp
@@ -0,0 +1,32 @@
+/* time complexity: O(n^2) */
+/* space complexity: O(1) */
+
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        constexpr int target = 0;
+        std::vector<std::vector<int>> res;
+        std::sort(nums.begin(), nums.end());
+        for (int i = 0; i < nums.size(); ++i) {
+            int j = i + 1, k = nums.size() - 1;
+            if (i >= 1 && nums[i] == nums[i - 1]) continue;
+            while (j < k) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) {
+                    ++j;
+                    continue;
+                }
+                if (k < nums.size() - 1 && nums[k] == nums[k + 1]) {
+                    --k;
+                    continue;
+                }
+                if (nums[i] + nums[j] + nums[k] > target)
+                    --k;
+                else if (nums[i] + nums[j] + nums[k] < target)
+                    ++j;
+                else if (nums[i] + nums[j] + nums[k] == target)
+                    res.emplace_back(std::vector<int>{nums[i], nums[j++], nums[k]});
+            }
+        }
+        return res;
+    }
+};
