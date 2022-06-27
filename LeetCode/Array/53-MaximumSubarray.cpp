53c5f2af0f10199408454a4e96857bcfb84f6ae1
jacklanda <yonyonlau@gmail.com>
2022-06-27 13:40:46 +0800

    add 53-MaximumSubarray

---
+++ LeetCode/Array/53-MaximumSubarray.cpp
@@ -0,0 +1,14 @@
+/* time complexity: O(n) */
+/* space complexity: O(1) */
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int sum = 0, max_sum = INT_MIN;
+        for (const int& num : nums) {
+            sum = std::max(sum + num, num);
+            max_sum = std::max(sum, max_sum);
+        }
+        return max_sum;
+    }
+};
