b80fa9c7516a8ee0781296c493ea11642caa4bff
jacklanda <yonyonlau@gmail.com>
2022-06-30 13:22:28 +0800

    update 739-DailyTemperatures

--- LeetCode/Stack/739-DailyTemperatures.cpp
+++ LeetCode/Stack/739-DailyTemperatures.cpp
@@ -22,3 +22,22 @@ public:
         return res;
     }
 };
+
+/* Array-based Approach */
+/* time complexity: O(n) */
+/* space complexity: O(n) */
+
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        std::vector<int> res(temperatures.size(), 0);
+        for (int i = temperatures.size() - 2; i >= 0; --i) {
+            int j = i + 1;  // start at next day
+            while (j < temperatures.size() and temperatures[j] <= temperatures[i])
+                j += res[j] ? res[j] : temperatures.size();  // iterate till you find next warmer day (NWD)
+            // if j >= days' length, NWD doesnt exist. Otherwise, assign distance to NWD
+            if (j < temperatures.size()) res[i] = j - i;
+        }
+        return res;
+    }
+};
