f0b0ff879ac530c79a82a5bdef5ad256f2c356c5
jacklanda <yonyonlau@gmail.com>
2022-07-03 12:17:14 +0800

    update 1539-KthMissingPositiveNumber

--- LeetCode/Array/1539-KthMissingPositiveNumber.cpp
+++ LeetCode/Array/1539-KthMissingPositiveNumber.cpp
@@ -9,3 +9,21 @@ public:
         return k;
     }
 };
+
+/* time complexity: O(log(n)) */
+/* space complexity: O(1) */
+
+class Solution {
+public:
+    int findKthPositive(vector<int>& arr, int k) {
+        int left = 0, right = arr.size();
+        while (left < right) {
+            int mid = left + ((right - left) >> 1);
+            if (arr[mid] - mid - 1 < k)
+                left = mid + 1;
+            else
+                right = mid;
+        }
+        return left + k;
+    }
+};
