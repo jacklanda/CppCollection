5753c47ffd221447b2bc3364a7c9af3a254b7d0e
jacklanda <yonyonlau@gmail.com>
2022-08-01 20:42:55 +0800

    add 463. Island Perimeter

---
+++ LeetCode/Array/463. Island Perimeter.cpp
@@ -0,0 +1,19 @@
+/* time complexity: O(n^2) */
+/* space complexity: O(1) */
+
+class Solution {
+public:
+    int islandPerimeter(vector<vector<int>>& grid) {
+        int count = 0, repeat = 0;
+        for (int i = 0; i < grid.size(); ++i) {
+            for (int j = 0; j < grid[0].size(); ++j) {
+                if (grid[i][j] == 1) {
+                    ++count;
+                    if (i != 0 and grid[i - 1][j] == 1) ++repeat;
+                    if (j != 0 and grid[i][j - 1] == 1) ++repeat;
+                }
+            }
+        }
+        return 4 * count - 2 * repeat;
+    }
+};
