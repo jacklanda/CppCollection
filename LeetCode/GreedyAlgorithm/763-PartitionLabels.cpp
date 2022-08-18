b11c15512b4aeab97ace561532edf046e26bb35f
jacklanda <yonyonlau@gmail.com>
2022-08-18 21:03:58 +0800

    Update 763. PartitionLabels

--- LeetCode/GreedyAlgorithm/763-PartitionLabels.cpp
+++ LeetCode/GreedyAlgorithm/763-PartitionLabels.cpp
@@ -1,22 +1,22 @@
 /* time complexity: O(n) */
-/* space complexity: O(k) */
+/* space complexity: O(1) */
 
 class Solution {
 public:
     vector<int> partitionLabels(string s) {
-        int char2pos[128] = {};
-        for (int i = 0; i < s.size(); ++i) char2pos[s[i]] = i;
+        int char2pos[26] = {0};
+        for (int i = 0; i < s.size(); ++i) char2pos[s[i] - 'a'] = i;
 
         vector<int> res;
-        int prev = -1, maximum = 0;
-
+        int left = 0, right = 0;
         for (int i = 0; i < s.size(); ++i) {
-            maximum = std::max(maximum, char2pos[s[i]]);
-            if (maximum == i) {
-                res.emplace_back(maximum - prev);
-                prev = maximum;
+            right = std::max(right, char2pos[s[i] - 'a']);
+            if (right == i) {
+                res.emplace_back(right - left + 1);
+                left = right + 1;
             }
         }
+
         return res;
     }
 };
