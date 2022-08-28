a86e3065ec5b9acb254fb186d2cf6b661d10ef24
jacklanda <yonyonlau@gmail.com>
2022-08-28 16:29:12 +0800

    Add 86. Partition List

---
+++ LeetCode/LinkedList/86. Partition List.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+/* time complexity: O(n) */
+/* space complexity: O(1) */
+class Solution {
+public:
+    ListNode* partition(ListNode* head, int x) {
+        ListNode* lt = new ListNode;
+        ListNode* ge = new ListNode;
+        ListNode* p1 = lt;
+        ListNode* p2 = ge;
+        
+        while (head) {
+            if (head->val < x) {
+                p1->next = head;
+                p1 = p1->next;
+            } else {
+                p2->next = head;
+                p2 = p2->next;
+            }
+            head = head->next;
+        }
+        p1->next = ge->next;
+        p2->next = nullptr;
+        
+        return lt->next;
+    }
+};
