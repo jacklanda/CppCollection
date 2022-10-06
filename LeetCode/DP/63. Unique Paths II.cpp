9031a15056a3794414bce0ecf817da4422db14ce
jacklanda <yonyonlau@gmail.com>
2022-10-06 18:52:38 +0800

    Add 63. Unique Paths II

---
+++ LeetCode/DP/63. Unique Paths II.cpp
@@ -0,0 +1,21 @@
+/* time complexity: O(m*n) */
+/* space complexity: O(m*n) */
+
+class Solution {
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        int m = obstacleGrid.size(), n = obstacleGrid[0].size();
+        if (obstacleGrid[0][0] == 1 || obstacleGrid[m - 1][n - 1] == 1)
+            return 0;
+        std::vector<std::vector<int>> dp(m, std::vector<int>(n, 0));
+        for (int i = 1; i < m && obstacleGrid[i][0] == 0; ++i) dp[i][0] = 1;
+        for (int j = 0; j < n && obstacleGrid[0][j] == 0; ++j) dp[0][j] = 1;
+        for (int i = 1; i < m; ++i) {
+            for (int j = 1; j < n; ++j) {
+                if (obstacleGrid[i][j] == 1) continue;
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+};
