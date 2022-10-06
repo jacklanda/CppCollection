1496d13e19a51feb674f8b0aec153f1cfff0f8f8
jacklanda <yonyonlau@gmail.com>
2022-10-06 18:08:25 +0800

    Add 62. Unique Paths

---
+++ LeetCode/DP/62. Unique Paths.cpp
@@ -0,0 +1,33 @@
+/* time complexity: O(m*n) */
+/* space complexity: O(m*n) */
+
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        std::vector<std::vector<int>> dp(m, std::vector<int>(n, 0));
+        for (int j = 0; j < n; ++j) dp[0][j] = 1;
+        for (int i = 0; i < m; ++i) dp[i][0] = 1;
+        for (int i = 1; i < m; ++i) {
+            for (int j = 1; j < n; ++j) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+};
+
+/* optimized space complexity solution */
+/* time complexity: O(m*n) */
+/* space complexity: O(n) */
+
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        std::vector<int> dp(n, 1);
+        for (int i = 1; i < m; ++i) {
+            for (int j = 1; j < n; ++j)
+                dp[j] += dp[j - 1];
+        }
+        return dp[n - 1];
+    }
+};
