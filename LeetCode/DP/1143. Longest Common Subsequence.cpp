98e338db05e07d2f5c2373959659114daed54bad
jacklanda <yonyonlau@gmail.com>
2022-10-22 14:46:06 +0800

    Update 1143. Longest Common Subsequence

--- LeetCode/DP/1143. Longest Common Subsequence.cpp
+++ LeetCode/DP/1143. Longest Common Subsequence.cpp
@@ -8,11 +8,10 @@ public:
         vector<vector<int>> dp(text1.size() + 1, vector<int>(text2.size() + 1, 0));
         for (int i = 1; i <= text1.size(); ++i) {
             for (int j = 1; j <= text2.size(); ++j) {
-                if (text1[i - 1] == text2[j - 1]) {
+                if (text1[i - 1] == text2[j - 1])
                     dp[i][j] = dp[i - 1][j - 1] + 1;
-                } else {
+                else
                     dp[i][j] = std::max(dp[i - 1][j], dp[i][j - 1]);
-                }
             }
         }
         return dp.back().back();
