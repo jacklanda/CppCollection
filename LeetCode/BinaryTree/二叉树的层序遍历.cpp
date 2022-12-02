13935e084be608b43e432775acba68113f715783
jacklanda <yonyonlau@gmail.com>
2022-12-02 21:29:07 +0800

    Rename `二叉树的层序遍历` to `102. Binary Tree Level Order Traversal`.

--- LeetCode/BinaryTree/二叉树的层序遍历.cpp
+++ LeetCode/BinaryTree/二叉树的层序遍历.cpp
@@ -6,33 +6,35 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 
-/* 解此题的基本思想是BSF
- * 即：广度优先搜索 */
+/* Breadth-First Search */
+/* time complexity: O(n) */
+/* space complexity: O(n) */
 class Solution {
 public:
     vector<vector<int>> levelOrder(TreeNode* root) {
         vector<vector<int>> vec;
-        if (!root) return vec;
+        if (root == nullptr)
+            return vec;
         queue<TreeNode*> q;
-        q.push(root);
-        /* 每一次while遍历都代表着层序遍历中的一层
-         * 也会在外层大的vector内压入一个新的vector元素 */
+        q.emplace(root);
+        
         while (!q.empty()) {
-            vec.push_back(vector<int>());
-            int element_num = q.size();
-            for (int i = 0; i < element_num; i++) {
+            int size = q.size();
+            vector<int> sub_vec;
+            for (int i = 0; i < size; ++i) {
                 TreeNode* node = q.front();
                 q.pop();
-                vec.back().push_back(node->val);
-                if (node->left) q.push(node->left);
-                if (node->right) q.push(node->right);
+                sub_vec.emplace_back(node->val);
+                if (node->left != nullptr) q.emplace(node->left);
+                if (node->right != nullptr) q.emplace(node->right);
             }
+            vec.emplace_back(sub_vec);
         }
+        
         return vec;
     }
 };
