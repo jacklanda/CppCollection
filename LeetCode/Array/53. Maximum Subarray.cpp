216499572a81bbeec8b6eef8b504af4b0c8d532c
jacklanda <yonyonlau@gmail.com>
2022-12-06 17:18:09 +0800

    Update `53. Maximum Subarray`.

--- LeetCode/Array/53. Maximum Subarray.cpp
+++ LeetCode/Array/53. Maximum Subarray.cpp
@@ -13,3 +13,19 @@ public:
         return max_sum;
     }
 };
+
+/* DP approach */
+/* time complexity: O(n) */
+/* space complexity: O(n) */
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        vector<int> dp(nums.size(), 0);
+        int res = dp[0] = nums[0];
+        for (int i = 1; i < nums.size(); ++i) {
+            dp[i] = std::max(dp[i - 1] + nums[i], nums[i]);
+            res = std::max(res, dp[i]);
+        }
+        return res;
+    }
+};
