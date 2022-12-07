35434c6210d3ba6634c07c1720b0cd4a62b14539
jacklanda <yonyonlau@gmail.com>
2022-12-07 17:13:54 +0800

    Update `122. Best Time to Buy and Sell Stock II`.

--- LeetCode/DP/122. Best Time to Buy and Sell Stock II.cpp
+++ LeetCode/DP/122. Best Time to Buy and Sell Stock II.cpp
@@ -25,10 +25,11 @@ public:
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int profit = 0;
+        int profit = 0, max_profit = 0;
         for (int i = 1; i < prices.size(); ++i) {
-            if (prices[i] > prices[i - 1]) profit += prices[i] - prices[i - 1];
+            profit = prices[i] - prices[i - 1];
+            max_profit = profit > 0 ? max_profit + profit : max_profit;
         }
-        return profit;
+        return max_profit;
     }
 };
