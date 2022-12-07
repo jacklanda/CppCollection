1b77c558780368f8dfaaed2c6f5717d212b7191d
jacklanda <yonyonlau@gmail.com>
2022-12-07 17:10:41 +0800

    Change dir & name for `122. Best Time to Buy and Sell Stock II`.

--- LeetCode/DP/122. Best Time to Buy and Sell Stock II.cpp
+++ LeetCode/DP/122. Best Time to Buy and Sell Stock II.cpp
@@ -5,13 +5,14 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int max_profit = 0;
         vector<int> buy_prices(prices.size(), 0);
         vector<int> profits(prices.size(), 0);
         buy_prices[0] = prices[0];
+        int profit = 0;
         for (int i = 1; i < prices.size(); ++i) {
+            profit = prices[i] - prices[i - 1];
             buy_prices[i] = std::min(buy_prices[i - 1], prices[i]);
-            profits[i] = std::max(profits[i - 1], prices[i] - buy_prices[i]);
+            profits[i] = profit > 0 ? profits[i - 1] + profit : profits[i - 1];
         }
         return profits.back();
     }
@@ -24,12 +25,10 @@ public:
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int max_profit = 0;
-        int min_buy_price = prices[0];
+        int profit = 0;
         for (int i = 1; i < prices.size(); ++i) {
-            min_buy_price = std::min(min_buy_price, prices[i]);
-            max_profit = std::max(max_profit, prices[i] - min_buy_price);
+            if (prices[i] > prices[i - 1]) profit += prices[i] - prices[i - 1];
         }
-        return max_profit;
+        return profit;
     }
 };
