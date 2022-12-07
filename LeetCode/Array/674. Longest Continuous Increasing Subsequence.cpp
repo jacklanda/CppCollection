b988d784d4c1ad7a3c0bd912dbb3762a25a0fcc9
jacklanda <yonyonlau@gmail.com>
2022-12-07 11:37:22 +0800

    Update `674. Longest Continuous Increasing Subsequence`.

--- LeetCode/Array/674. Longest Continuous Increasing Subsequence.cpp
+++ LeetCode/Array/674. Longest Continuous Increasing Subsequence.cpp
@@ -20,3 +20,20 @@ public:
         return max_cnt;
     }
 };
+
+/* DP approach */
+/* time complexity: O(n) */
+/* space complexity: O(n) */
+class Solution {
+public:
+    int findLengthOfLCIS(vector<int>& nums) {
+        int res = 1;
+        vector<int> dp(nums.size(), 1);
+        for (int i = 1; i < nums.size(); ++i) {
+            if (nums[i] > nums[i - 1])
+                dp[i] = dp[i - 1] + 1;
+            res = std::max(dp[i], res);
+        }
+        return res;
+    }
+};
