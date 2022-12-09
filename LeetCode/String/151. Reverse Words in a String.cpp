a473cea6ee9017b11f0acae25563aab2698c2602
jacklanda <yonyonlau@gmail.com>
2022-12-09 12:55:50 +0800

    Update `27. Remove Element` and `151. Reverse Words in a String`.

--- LeetCode/String/151. Reverse Words in a String.cpp
+++ LeetCode/String/151. Reverse Words in a String.cpp
@@ -2,32 +2,31 @@
 /* space complexity: O(1) */
 
 class Solution {
-public:
-    string reverseWords(string s) {
-        remove_extra_spaces(s);
-        reverse(s, 0, s.size() - 1);
-        for (int i = 0, j = 0; i <= s.size(); ++i) {
-            if (s[i] == ' ' or i == s.size()) {
-                reverse(s, j, i - 1);
-                j = i + 1;
-            }
-        }
-        return s;
-    }
 private:
-    void remove_extra_spaces(string& s) {
-        int slow = 0;
+    void removeSpaces(string& s) {
+        int idx = 0;
         for (int i = 0; i < s.size(); ++i) {
             if (s[i] != ' ') {
-                if (slow != 0) s[slow++] = ' ';
-                while (s[i] != ' ' and i < s.size()) s[slow++] = s[i++];
+                if (idx != 0) s[idx++] = ' ';
+                while (i < s.size() && s[i] != ' ') s[idx++] = s[i++];
             }
         }
-        s.resize(slow);
+        s.resize(idx);
     }
-    
-    void reverse(string& s, int left, int right) {
-        while (left < right)
-            std::swap(s[left++], s[right--]);
+
+    void reverseWordbyWord(string& s) {
+        for (int i = 0, j = 0; j <= s.size(); ++j) {
+            if (j == s.size() || s[j] == ' ') {
+                std::reverse(s.begin() + i, s.begin() + j);
+                i = j + 1;
+            }
+        }
+    }
+public:
+    string reverseWords(string s) {
+        removeSpaces(s);  // remove redundant space
+        std::reverse(s.begin(), s.end());  // reverse the all string
+        reverseWordbyWord(s);  // reverse word separated by a single space
+        return s;
     }
 };
