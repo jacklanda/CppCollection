f136321e6e78e2afcef5ba1a98af97ec3f83807a
jacklanda <yonyonlau@gmail.com>
2022-12-12 12:00:10 +0800

    Update `62. Unique Paths`.

--- LeetCode/DP/62. Unique Paths.cpp
+++ LeetCode/DP/62. Unique Paths.cpp
@@ -4,15 +4,13 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        std::vector<std::vector<int>> dp(m, std::vector<int>(n, 0));
-        for (int j = 0; j < n; ++j) dp[0][j] = 1;
-        for (int i = 0; i < m; ++i) dp[i][0] = 1;
+        vector<vector<int>> dp(m, vector<int>(n, 1));
         for (int i = 1; i < m; ++i) {
             for (int j = 1; j < n; ++j) {
                 dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
             }
         }
-        return dp[m - 1][n - 1];
+        return dp.back().back();
     }
 };
 
