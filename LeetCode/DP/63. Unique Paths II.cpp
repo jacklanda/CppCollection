30e6dd7f71ac8729288a6cafa408a2725e94e292
jacklanda <yonyonlau@gmail.com>
2022-12-12 14:38:07 +0800

    Add `96. Unique Binary Search Trees`.

--- LeetCode/DP/63. Unique Paths II.cpp
+++ LeetCode/DP/63. Unique Paths II.cpp
@@ -8,7 +8,7 @@ public:
         if (obstacleGrid[0][0] == 1 || obstacleGrid[m - 1][n - 1] == 1)
             return 0;
         std::vector<std::vector<int>> dp(m, std::vector<int>(n, 0));
-        for (int i = 1; i < m && obstacleGrid[i][0] == 0; ++i) dp[i][0] = 1;
+        for (int i = 0; i < m && obstacleGrid[i][0] == 0; ++i) dp[i][0] = 1;
         for (int j = 0; j < n && obstacleGrid[0][j] == 0; ++j) dp[0][j] = 1;
         for (int i = 1; i < m; ++i) {
             for (int j = 1; j < n; ++j) {
